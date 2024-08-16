3b3782604542f0b132be90e7ba82dc2638e42f0d
Trong Lam <tronglam199015@gmail.com>
2024-08-16 16:18:02 +0700

    init microservices

---
+++ ChatAppBE(Microservices)/friendship-service/src/main/java/com/microservices/friendship/FriendshipServiceApplication.java
@@ -0,0 +1,13 @@
+package com.microservices.friendship;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FriendshipServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FriendshipServiceApplication.class, args);
+	}
+
+}
---
+++ ChatAppBE(Microservices)/friendship-service/src/test/java/com/microservices/friendship/FriendshipServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.microservices.friendship;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FriendshipServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
