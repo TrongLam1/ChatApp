29c46410a132b29e47f5c8dc8aa16363491a3cce
Trong Lam <tronglam199015@gmail.com>
2024-11-18 16:45:59 +0700

    Add user and friendship api

---
+++ ChatApp-NextJS/src/app/api/friendshipApi.ts
@@ -0,0 +1,60 @@
+import { sendRequest } from "@/utils/api";
+
+export async function RequestFriend(token: string, body: IFriendship) {
+    return await sendRequest<IBackendRes<any>>({
+        url: `${process.env.NEXT_PUBLIC_BACKEND_URL}/friendship/request`,
+        method: 'POST',
+        headers: { Authorization: `Bearer ${token}` },
+        body
+    });
+};
+
+export async function AcceptFriend(token: string, body: IFriendship) {
+    return await sendRequest<IBackendRes<any>>({
+        url: `${process.env.NEXT_PUBLIC_BACKEND_URL}/friendship/accept`,
+        method: 'PUT',
+        headers: { Authorization: `Bearer ${token}` },
+        body
+    });
+};
+
+export async function CancelFriend(token: string, body: IFriendship) {
+    return await sendRequest<IBackendRes<any>>({
+        url: `${process.env.NEXT_PUBLIC_BACKEND_URL}/friendship/cancel`,
+        method: 'DELETE',
+        headers: { Authorization: `Bearer ${token}` },
+        body
+    });
+};
+
+export async function FindFriend(token: string, friendId: string) {
+    return await sendRequest<IBackendRes<any>>({
+        url: `${process.env.NEXT_PUBLIC_BACKEND_URL}/friendship/find-friend/${friendId}`,
+        method: 'GET',
+        headers: { Authorization: `Bearer ${token}` },
+    });
+};
+
+export async function GetListFriends(token: string) {
+    return await sendRequest<IBackendRes<any>>({
+        url: `${process.env.NEXT_PUBLIC_BACKEND_URL}/friendship/list-friends`,
+        method: 'GET',
+        headers: { Authorization: `Bearer ${token}` },
+    });
+};
+
+export async function GetListRequestFriends(token: string) {
+    return await sendRequest<IBackendRes<any>>({
+        url: `${process.env.NEXT_PUBLIC_BACKEND_URL}/friendship/list-request-friends`,
+        method: 'GET',
+        headers: { Authorization: `Bearer ${token}` },
+    });
+};
+
+export async function CountRequestFriends(token: string) {
+    return await sendRequest<IBackendRes<any>>({
+        url: `${process.env.NEXT_PUBLIC_BACKEND_URL}/friendship/count-request-friends`,
+        method: 'GET',
+        headers: { Authorization: `Bearer ${token}` },
+    });
+};
--- ChatApp-NextJS/src/app/api/userApi.ts
+++ ChatApp-NextJS/src/app/api/userApi.ts
@@ -1,2 +1,39 @@
 'use server'
 
+import { sendRequest } from "@/utils/api";
+
+export async function Register(username: string, email: string, password: string) {
+    return await sendRequest<IBackendRes<any>>({
+        url: `${process.env.NEXT_PUBLIC_BACKEND_URL}/auth/register`,
+        method: 'POST',
+        body: {
+            username, email, password
+        }
+    });
+}
+
+export async function GetProfile(token: string) {
+    return await sendRequest<IBackendRes<any>>({
+        url: `${process.env.NEXT_PUBLIC_BACKEND_URL}/users`,
+        method: 'GET',
+        headers: { Authorization: `Bearer ${token}` },
+    });
+}
+
+export async function UpdateProfile(token: string, body: IProfile) {
+    return await sendRequest<IBackendRes<any>>({
+        url: `${process.env.NEXT_PUBLIC_BACKEND_URL}/users/update-profile`,
+        method: 'PUT',
+        headers: { Authorization: `Bearer ${token}` },
+        body
+    });
+}
+
+export async function ChangeAvatar(token: string, file: any) {
+    return await sendRequest<IBackendRes<any>>({
+        url: `${process.env.NEXT_PUBLIC_BACKEND_URL}/users/change-avatar`,
+        method: 'PUT',
+        headers: { Authorization: `Bearer ${token}` },
+        body: { file }
+    });
+}
